6cce1f96f8b20338b95706b6b5335c371957579d
sameera <sk1234@wildcats.unh.edu>
2021-03-16 20:06:41 -0400

    project structure

---
+++ complex-cnn.py
@@ -0,0 +1,56 @@
+#importing the libraries
+import numpy
+from tensorflow.python.keras.datasets import mnist
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import Dense
+from tensorflow.python.keras.layers import Dropout
+from tensorflow.python.keras.layers import Flatten
+from tensorflow.python.keras.layers.convolutional import Conv2D
+from tensorflow.python.keras.layers.convolutional import MaxPooling2D
+from tensorflow.keras.optimizers import Adam
+from tensorflow.python.keras.utils import np_utils
+import os
+import tensorflow as tf
+tf.compat.v1.disable_eager_execution()
+
+# fix random seed for reproducibility
+seed = 7
+numpy.random.seed(seed)
+
+# load data
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+# Reshaping to format which CNN expects (batch, height, width, channels)
+X_train = X_train.reshape(X_train.shape[0], X_train.shape[1], X_train.shape[2], 1).astype('float32')
+X_test = X_test.reshape(X_test.shape[0], X_test.shape[1], X_test.shape[2], 1).astype('float32')
+
+# normalize inputs from 0-255 to 0-1
+X_train/=255
+X_test/=255
+
+# one hot encode
+number_of_classes = 10
+y_train = np_utils.to_categorical(y_train, number_of_classes)
+y_test = np_utils.to_categorical(y_test, number_of_classes)
+
+#creating model
+model = Sequential()
+model.add(Conv2D(32, (5, 5), input_shape=(X_train.shape[1], X_train.shape[2], 1), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(32, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.2))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dense(number_of_classes, activation='softmax'))
+
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_size=200)
+
+# Save the model
+model.save('mnist-flask/models/mnist-denseCNN.h5')
+
+metrics = model.evaluate(X_test, y_test, verbose=0)
+print("Metrics(Test loss & Test Accuracy): ")
+print(metrics)
---
+++ mnist-flask/app.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request
+from PIL import Image
+import numpy as np
+from tensorflow.python.keras.datasets import mnist
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import Dense
+from tensorflow.python.keras.layers import Dropout
+from tensorflow.python.keras.layers import Flatten
+from tensorflow.python.keras.layers.convolutional import Conv2D
+from tensorflow.python.keras.layers.convolutional import MaxPooling2D
+from tensorflow.keras.optimizers import Adam
+from tensorflow.python.keras.utils import np_utils
+import os
+import tensorflow as tf
+from tensorflow.python.keras.models import load_model
+
+
+app = Flask(__name__, template_folder='templates')
+
+def init():
+    global model,graph
+    # load the pre-trained Keras model
+    model = load_model('./models/mnist-dense.h5')
+    graph = tf.compat.v1.get_default_graph()
+
+@app.route('/')
+def upload_file():
+   return render_template('index.html')
+	
+@app.route('/uploader', methods = ['POST'])
+def upload_image_file():
+   if request.method == 'POST':
+        img = Image.open(request.files['file'].stream).convert("L")
+        img = img.resize((28,28))
+        im2arr = np.array(img)
+        im2arr = im2arr.reshape(1,28,28,1)
+        y_pred = np.argmax(model.predict(im2arr), axis=-1)
+
+        return 'Predicted Number: ' + str(y_pred[0])
+		
+if __name__ == '__main__':
+    print(("* Loading Keras model and Flask starting server..."
+        "please wait until server has fully started"))
+    init()
+    app.run(debug = True)
